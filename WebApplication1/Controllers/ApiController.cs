bc5197ee41d43690dc574a5b18cbf90e06b089e6
oooyuuooo <shiyu9082@gmail.com>
2024-01-23 16:45:32 +0800

    2024/01/23

--- WebApplication1/Controllers/ApiController.cs
+++ WebApplication1/Controllers/ApiController.cs
@@ -4,22 +4,59 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-	public class ApiController : Controller
-	{
-		private readonly MyDBContext _dbContext;
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
         public ApiController(MyDBContext dbContext)
         {
             _dbContext = dbContext;
         }
-        public IActionResult Index()
-		{
-			return View();
-		}
 
-		public IActionResult Cities()
-		{
-			var cities = _dbContext.Addresses.Select(c => c.City).Distinct();
-			return Json(cities);
-		}
-	}
+        //[HttpPost] error 505
+        public IActionResult Index()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return Content("Hello Content");
+            //return Content("<h2>Hello!!!</h2>", "text/html");
+            //return Content("Hello 你好 我是歐歐歐","text/plain",System.Text.Encoding.UTF8);
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(c => c.City).Distinct();
+            return Json(cities);
+        }
+
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _dbContext.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                return File(img, "image/jpeg");
+            }
+            return NotFound();
+        }
+
+        public IActionResult Register(string name, int age = 26)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Guest";
+            }
+            return Content($"Hello {name}, You are {age} years old");
+        }
+
+        public IActionResult Districts(string city)
+        {
+            var distircts = _dbContext.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
+            return Json(distircts);
+        }
+
+        public IActionResult Roads(string siteId)
+        {
+            var road = _dbContext.Addresses.Where(a => a.SiteId == siteId).Select(a => a.Road).Distinct();
+            return Json(road);
+        }
+    }
 }
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -28,5 +28,20 @@ namespace WebApplication1.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		public IActionResult First()
+		{
+			return View();
+		}
+
+		public IActionResult Address()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
 	}
 }
